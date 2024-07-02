04c30ebd34cbf0f3b034c17ff16d824d684dd38a
Mohammed2579m <148061288+Mohammed2579m@users.noreply.github.com>
2024-07-02 15:07:48 +0300

    M7 BOT

---
+++ bot.js
@@ -0,0 +1,17 @@
+javascript
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const token = 'YOUR_BOT_TOKEN';
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(token);
